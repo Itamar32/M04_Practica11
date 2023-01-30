abee965a78289090df0039faf6f29653fe69737d
itamar <itamar@example.com>
2023-01-30 20:36:28 +0100

    practica itamar

---
+++ main.py
@@ -0,0 +1,7 @@
+import Exercici1_A as e1
+import Exercici2_A as e2
+import Exercici3_A as e3
+
+e1.mayor()    #Ejercicio sobre que numero es mayor que otro.
+e2.decide()   #Ejercicio sobre las 5 palabras.
+e3.numeros()  #Ejercicio para encontrar un numero entre 0 y 100.
